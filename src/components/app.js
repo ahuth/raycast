d67c93a13fdeb91afec0ff6ff74ab641b34c230c
Andrew Huth <ahuth@mavenlink.com>
2017-05-21 10:08:51 -0700

    Make /components directory

---
+++ src/components/app.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function App() {
+  return (
+    <div>
+      <div>
+        <h2>Welcome to React</h2>
+      </div>
+      <p>
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+  )
+}
