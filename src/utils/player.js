00bf86bb78c3afbc0059b9216f4772a5ecb95544
Andrew Huth <ahuth@mavenlink.com>
2017-05-21 10:25:38 -0700

    Add function to allow the player to "walk"

--- src/utils/player.js
+++ src/utils/player.js
@@ -3,3 +3,10 @@ export default function Player(x, y, direction) {
   this.y = y
   this.direction = direction
 }
+
+Player.prototype.walk = function (distance, map) {
+  const dx = Math.cos(this.direction) * distance
+  const dy = Math.sin(this.direction) * distance
+  this.x += dx
+  this.y += dy
+}
