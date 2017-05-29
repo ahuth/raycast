4db38884e70cacbc2c435061ad8f82b92c898091
Andrew Huth <ahuth@mavenlink.com>
2017-05-29 10:14:21 -0700

    Remove unused Player functions

--- src/utils/player.js
+++ src/utils/player.js
@@ -4,13 +4,3 @@ export default function Player(x, y, height, direction) {
   this.height = height
   this.direction = direction
 }
-
-Player.prototype.walk = function (distance, map) {
-  const dx = Math.cos(this.direction) * distance
-  const dy = Math.sin(this.direction) * distance
-  this.x += dx
-  this.y += dy
-}
-
-Player.prototype.update = function (map, seconds) {
-}
