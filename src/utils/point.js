2b6cf1988f0a56479e76a78e9a9697cc6394d535
Andrew Huth <ahuth@mavenlink.com>
2017-06-03 19:54:53 -0700

    Whitespace

--- src/utils/point.js
+++ src/utils/point.js
@@ -5,7 +5,7 @@ export default function Point(x, y) {
 
 // Convert unit coordinates to grid coordinates. Each grid coordinate can be broken up into
 // some number of smaller "unit" coordinates.
-Point.prototype.toGrid = function(unitsPerGrid) {
+Point.prototype.toGrid = function (unitsPerGrid) {
   return new Point(Math.floor(this.x / unitsPerGrid), Math.floor(this.y / unitsPerGrid))
 }
 
