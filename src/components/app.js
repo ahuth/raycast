a2dd0bd7ed8e087cb1d1c7fdd85e62a74ac0f682
Andrew Huth <ahuth@mavenlink.com>
2017-06-04 17:48:35 -0700

    Set the initial player direction back to 0 degrees to make visualization easier

--- src/components/app.js
+++ src/components/app.js
@@ -12,7 +12,7 @@ export default function App() {
         resolution={320}
         fov={fromDegrees(60)}
         map={new Map(10, 64).room()}
-        player={new Player(160, 160, 32, fromDegrees(-20))}
+        player={new Player(160, 160, 32, fromDegrees(0))}
         height={400}
         width={720}
       />
