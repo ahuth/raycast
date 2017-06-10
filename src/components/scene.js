fcf02b999fdf72be09dffed180f4914d92e1c734
Andrew Huth <ahuth@mavenlink.com>
2017-06-09 20:41:20 -0700

    Setup hotkeys to let the player move around

--- src/components/scene.js
+++ src/components/scene.js
@@ -1,4 +1,5 @@
 import PropTypes from "prop-types"
+import Mousetrap from "mousetrap"
 import React from "react"
 import Column from "./column"
 
@@ -7,7 +8,9 @@ export default class Scene extends React.Component {
     super(props)
     this.update = this.update.bind(this)
     this.renderScene = this.renderScene.bind(this)
-    this.state = {columns: []}
+    this.handleKeyDown = this.handleKeyDown.bind(this)
+    this.handleKeyUp = this.handleKeyUp.bind(this)
+    this.state = {columns: [], keys: {}}
     this.styles = {
       height: props.height,
       width: props.width,
@@ -18,6 +21,8 @@ export default class Scene extends React.Component {
 
   componentWillMount() {
     this.context.loop.subscribe(this.update)
+    Mousetrap.bind(["w", "s", "a", "d"], this.handleKeyDown, "keydown")
+    Mousetrap.bind(["w", "s", "a", "d"], this.handleKeyUp, "keyup")
   }
 
   componentDidMount() {
@@ -26,9 +31,26 @@ export default class Scene extends React.Component {
 
   componentWillUnmount() {
     this.context.loop.unsubscribe(this.update)
+    Mousetrap.unbind(["w", "s", "a", "d"], "keydown")
+    Mousetrap.unbind(["w", "s", "a", "d"], "keyup")
   }
 
   update(seconds) {
+    if (this.state.w || this.state.s || this.state.a || this.state.d) {
+      if (this.state.w) { this.props.player.moveForward() }
+      if (this.state.s) { this.props.player.moveBackward() }
+      if (this.state.a) { this.props.player.turnLeft() }
+      if (this.state.d) { this.props.player.turnRight() }
+      this.renderScene()
+    }
+  }
+
+  handleKeyDown(event) {
+    this.setState({[event.key]: true})
+  }
+
+  handleKeyUp(event) {
+    this.setState({[event.key]: false})
   }
 
   renderScene() {
