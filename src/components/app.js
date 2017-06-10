4c70d48ba401b3de943fd67ada07ca55ee15728c
Andrew Huth <ahuth@mavenlink.com>
2017-06-10 11:54:03 -0700

    Add some basic instructions

--- src/components/app.js
+++ src/components/app.js
@@ -7,15 +7,18 @@ import {fromDegrees} from "../utils/radians"
 
 export default function App() {
   return (
-    <Loop>
-      <Scene
-        resolution={320}
-        fov={fromDegrees(60)}
-        map={new Map(10, 64).room()}
-        player={new Player(160, 160, 32, fromDegrees(0))}
-        height={400}
-        width={720}
-      />
-    </Loop>
+    <div>
+      <Loop>
+        <Scene
+          resolution={320}
+          fov={fromDegrees(60)}
+          map={new Map(10, 64).room()}
+          player={new Player(160, 160, 32, fromDegrees(0))}
+          height={400}
+          width={720}
+        />
+      </Loop>
+      <span>Move using the w, s, a, and d keys</span>
+    </div>
   )
 }
