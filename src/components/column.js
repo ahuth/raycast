9eec76e238d664f68cf2ca0f15871faadcea3099
Andrew Huth <ahuth@mavenlink.com>
2017-06-09 19:22:51 -0700

    Simplfy some math

--- src/components/column.js
+++ src/components/column.js
@@ -5,7 +5,7 @@ import React from "react"
 export default function Column({color, distance, mapHeight, number, resolution, screenHeight, screenWidth}) {
   const height = Math.min(mapHeight / distance * 255, screenHeight)
   const width = screenWidth / resolution
-  const top = (screenHeight / 2) - (height / 2)
+  const top = (screenHeight - height) / 2
   const adjustedColor = Color(color).darken(distance / 600).hex()
   const styles = {
     backgroundColor: adjustedColor,
