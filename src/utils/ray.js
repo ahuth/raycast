f3ed0f914d018529e9cecbf65f6c112823848914
Andrew Huth <ahuth@mavenlink.com>
2017-06-10 10:48:38 -0700

    Simplify wording of some comments

--- src/utils/ray.js
+++ src/utils/ray.js
@@ -7,7 +7,7 @@ export default function Ray(map, angle, x, y) {
   this.angle = normalize(angle)
 }
 
-// Determine the distance this ray will travel before hitting a wall.
+// Determine the distance travelled before hitting a wall.
 Ray.prototype.cast = function () {
   // Determine the direction the ray is travelling.
   const up = this.angle > 0 && this.angle < Math.PI
@@ -20,9 +20,9 @@ Ray.prototype.cast = function () {
   return Math.min(horizontalDistance, verticalDistance)
 }
 
-// Determine the distance the ray will travel before hitting a _horizontal_ wall.
+// Determine the distance travelled before hitting a _horizontal_ wall.
 function castHorizontal(map, origin, angle, up, right) {
-  // Calculate the coordinates of the first horizontal intersection with a grid boundary.
+  // Calculate the coordinates of the first intersection with a grid boundary.
   const intersectionY = Math.floor(origin.y / map.height) * map.height + (up ? -1 : map.height)
   const intersectionX = origin.x + (origin.y - intersectionY) / Math.tan(angle)
   const intersection = new Point(intersectionX, intersectionY)
@@ -34,9 +34,9 @@ function castHorizontal(map, origin, angle, up, right) {
   return wall ? wall.distance(origin) : Infinity
 }
 
-// Determine the distance the ray will travel before hitting a _vertical_ wall.
+// Determine the distance travelled before hitting a _vertical_ wall.
 function castVertical(map, origin, angle, up, right) {
-  // Calculate the coordinates of the first vertical intersection with a grid boundary.
+  // Calculate the coordinates of the first intersection with a grid boundary.
   const intersectionX = Math.floor(origin.x / map.height) * map.height + (right ? map.height : -1)
   const intersectionY = origin.y + (origin.x - intersectionX) * Math.tan(angle)
   const intersection = new Point(intersectionX, intersectionY)
