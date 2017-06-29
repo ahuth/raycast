657798d7ef6c61f8f697103c345f47a314cd00a9
Andrew Huth <ahuth@mavenlink.com>
2017-06-28 23:28:13 -0700

    Inline logic to convert from "unit" coordinates to the map's "grid" coordinates into the Map object
    No need to know about this in other objects.

--- src/utils/map.js
+++ src/utils/map.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-extend-native */
 
+import Point from "./point"
+
 export default function Map(height) {
   this.height = height
   this.size = 10
@@ -18,11 +20,17 @@ export default function Map(height) {
 }
 
 Map.prototype.isWall = function (point) {
-  const gridCoordinates = point.toGrid(this.height);
+  const gridCoordinates = toGrid(point, this.height)
   return this.isWithinBounds(gridCoordinates) && this.grid[gridCoordinates.y][gridCoordinates.x] === 1
 }
 
 Map.prototype.isWithinBounds = function (point) {
-  const gridCoordinates = point.toGrid(this.height);
+  const gridCoordinates = toGrid(point, this.height)
   return gridCoordinates.x >= 0 && gridCoordinates.x < this.size && gridCoordinates.y >=0 && gridCoordinates.y < this.size
+}
+
+// Convert unit coordinates to grid coordinates. Each grid coordinate can be broken up into
+// some number of smaller "unit" coordinates.
+function toGrid(point, unitsPerGrid) {
+  return new Point(Math.floor(point.x / unitsPerGrid), Math.floor(point.y / unitsPerGrid))
 }
--- src/utils/point.js
+++ src/utils/point.js
@@ -3,12 +3,6 @@ export default function Point(x, y) {
   this.y = y
 }
 
-// Convert unit coordinates to grid coordinates. Each grid coordinate can be broken up into
-// some number of smaller "unit" coordinates.
-Point.prototype.toGrid = function (unitsPerGrid) {
-  return new Point(Math.floor(this.x / unitsPerGrid), Math.floor(this.y / unitsPerGrid))
-}
-
 // Return a new point with the values incremented by the passed-in values.
 Point.prototype.add = function (x, y) {
   return new Point(this.x + x, this.y + y)
