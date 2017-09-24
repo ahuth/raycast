bb8f4b7c799be1d763c872d44b51843126871332
Andrew Huth <ahuth@mavenlink.com>
2017-09-24 14:01:05 -0700

    Annotate the <Column> component

--- src/components/app.js
+++ src/components/app.js
@@ -1,3 +1,5 @@
+// @flow
+
 import React from "react"
 import Loop from "../utils/loop"
 import Map from "../utils/map"
--- src/components/column.js
+++ src/components/column.js
@@ -1,8 +1,19 @@
+// @flow
+
 import Color from "color"
-import PropTypes from "prop-types"
 import React from "react"
 
-export default function Column({color, distance, mapHeight, number, resolution, screenHeight, screenWidth}) {
+type Props = {
+  color: string,
+  distance: number,
+  mapHeight: number,
+  number: number,
+  resolution: number,
+  screenHeight: number,
+  screenWidth: number,
+}
+
+export default function Column({color, distance, mapHeight, number, resolution, screenHeight, screenWidth}: Props) {
   const height = Math.min(mapHeight / distance * 255, screenHeight)
   const width = screenWidth / resolution
   const top = (screenHeight - height) / 2
@@ -16,14 +27,4 @@ export default function Column({color, distance, mapHeight, number, resolution, 
     top: top
   }
   return <div style={styles}></div>
-}
-
-Column.propTypes = {
-  color: PropTypes.string.isRequired,
-  distance: PropTypes.number.isRequired,
-  mapHeight: PropTypes.number.isRequired,
-  number: PropTypes.number.isRequired,
-  resolution: PropTypes.number.isRequired,
-  screenHeight: PropTypes.number.isRequired,
-  screenWidth: PropTypes.number.isRequired
 }
