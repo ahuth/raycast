def2e17fc2c7913a9b2c1355536388cd8dc4c154
Andrew Huth <ahuth@mavenlink.com>
2018-05-28 10:41:46 -0700

    Add whitespace for readability

--- src/utils/player.js
+++ src/utils/player.js
@@ -16,12 +16,15 @@ Player.prototype.castRays = function (map, fov, resolution) {
   // If the field of view is 60 degrees and the resolution is 320, there is 60 / 320 degrees
   // between each ray.
   const angleBetweenRays = fov / resolution;
+
   // The player's direction is the center of the screen, and the left edge of the screen is half
   // the field of view to the left. In our coordinate system, angles increase as we turn counter-
   // clockwise, so we add to player's current direction.
   const startAngle = this.direction + fov / 2;
+
   // Generate the angle for each ray starting from the left and sweeping to the right screen edge.
   const rayAngles = new Array(resolution).fill(0).map((_, index) => startAngle - index * angleBetweenRays);
+
   // Calculate the distance from each ray to the nearest wall.
   return rayAngles.map(angle => this.castRay(map, angle));
 };
@@ -30,6 +33,7 @@ Player.prototype.castRays = function (map, fov, resolution) {
 Player.prototype.castRay = function (map, angle) {
   // Find the raw distance to the nearest wall.
   const distance = new Ray(map, angle, this.position).cast();
+
   // Correct for a fishbowl-effect resulting from mixing polar and cartesian coordinates.
   return distance * Math.cos(angle - this.direction);
 };
@@ -45,6 +49,7 @@ Player.prototype.turnLeft = function () {
 Player.prototype.moveForward = function (map) {
   const deltaX = stepDistance * Math.cos(this.direction);
   const deltaY = stepDistance * Math.sin(this.direction);
+
   this.position = this.position.add(
     adjustDelta(map, this.position.add(deltaX, 0), deltaX),
     adjustDelta(map, this.position.add(0, -deltaY), -deltaY),
@@ -54,6 +59,7 @@ Player.prototype.moveForward = function (map) {
 Player.prototype.moveBackward = function (map) {
   const deltaX = stepDistance * Math.cos(this.direction);
   const deltaY = stepDistance * Math.sin(this.direction);
+
   this.position = this.position.add(
     adjustDelta(map, this.position.add(-deltaX, 0), -deltaX),
     adjustDelta(map, this.position.add(0, deltaY), deltaY),
@@ -64,6 +70,7 @@ Player.prototype.moveBackward = function (map) {
 Player.prototype.moveLeft = function (map) {
   const deltaX = stepDistance * Math.cos(this.direction + Math.PI / 2);
   const deltaY = stepDistance * Math.sin(this.direction + Math.PI / 2);
+
   this.position = this.position.add(
     adjustDelta(map, this.position.add(deltaX, 0), deltaX),
     adjustDelta(map, this.position.add(0, -deltaY), -deltaY),
@@ -74,6 +81,7 @@ Player.prototype.moveLeft = function (map) {
 Player.prototype.moveRight = function (map) {
   const deltaX = stepDistance * Math.cos(this.direction + Math.PI / 2);
   const deltaY = stepDistance * Math.sin(this.direction + Math.PI / 2);
+
   this.position = this.position.add(
     adjustDelta(map, this.position.add(-deltaX, 0), -deltaX),
     adjustDelta(map, this.position.add(0, deltaY), deltaY),
--- src/utils/ray.js
+++ src/utils/ray.js
@@ -12,10 +12,13 @@ Ray.prototype.cast = function () {
   // Determine the direction the ray is travelling.
   const up = this.angle > 0 && this.angle < Math.PI;
   const right = this.angle < (twoPi * 0.25) || this.angle > (twoPi * 0.75);
+
   // Determine the distance to the first horizontal wall.
   const horizontalDistance = castHorizontal(this.map, this.origin, this.angle, up, right);
+
   // Determine the distance to the first vertical wall.
   const verticalDistance = castVertical(this.map, this.origin, this.angle, up, right);
+
   // Return the shortest distance between the horizontal and vertical distances.
   return Math.min(horizontalDistance, verticalDistance);
 };
@@ -26,9 +29,11 @@ function castHorizontal(map, origin, angle, up, right) {
   const intersectionY = Math.floor(origin.y / map.height) * map.height + (up ? -0.01 : map.height);
   const intersectionX = origin.x + (origin.y - intersectionY) / Math.tan(angle);
   const intersection = new Point(intersectionX, intersectionY);
+
   // Calculate the change in x and y coordinates needed to iterate across grid boundaries.
   const deltaY = up ? -map.height : map.height;
   const deltaX = Math.abs(map.height / Math.tan(angle)) * (right ? 1 : -1);
+
   // Find the nearest wall and return the distance to it.
   const wall = findWall(map, intersection, deltaX, deltaY);
   return wall.distance(origin);
@@ -40,9 +45,11 @@ function castVertical(map, origin, angle, up, right) {
   const intersectionX = Math.floor(origin.x / map.height) * map.height + (right ? map.height : -0.01);
   const intersectionY = origin.y + (origin.x - intersectionX) * Math.tan(angle);
   const intersection = new Point(intersectionX, intersectionY);
+
   // Calculate the change in x and y coordinates needed to iterate across grid boundaries.
   const deltaX = right ? map.height : -map.height;
   const deltaY = Math.abs(map.height * Math.tan(angle)) * (up ? -1 : 1);
+
   // Find the nearest wall and return the distance to it.
   const wall = findWall(map, intersection, deltaX, deltaY);
   return wall.distance(origin);
@@ -54,10 +61,12 @@ function findWall(map, position, deltaX, deltaY) {
   if (!map.isWithinBounds(position)) {
     return new Point(Infinity, Infinity);
   }
+
   // If the ray has hit a wall, return its position.
   if (map.isWall(position)) {
     return position;
   }
+
   // No wall has been encountered. Iterate to the next grid boundary and check again.
   return findWall(map, position.add(deltaX, deltaY), deltaX, deltaY);
 }
