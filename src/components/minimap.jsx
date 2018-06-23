1c968b1b94d1b883e23834dc15384aba031c47f1
Andrew Huth <andrew@huth.me>
2018-06-23 12:39:46 -0700

    Show initial position of player on minimap

--- src/components/minimap.jsx
+++ src/components/minimap.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
 export default class Minimap extends React.Component {
-  canvasRef = React.createRef()
+  wallsRef = React.createRef()
+  playerRef = React.createRef()
 
   componentDidMount() {
     this.drawWalls();
+    this.drawPlayer();
   }
 
   drawWalls() {
-    const context = this.canvasRef.current.getContext('2d');
+    const context = this.wallsRef.current.getContext('2d');
     const { map, size } = this.props;
     const cellSize = size / map.size;
 
@@ -21,8 +23,37 @@ export default class Minimap extends React.Component {
     });
   }
 
+  drawPlayer() {
+    const context = this.playerRef.current.getContext('2d');
+    const { map, player, size } = this.props;
+    const { position } = player;
+    const gridX = position.x / map.height;
+    const gridY = position.y / map.height;
+    const cellSize = size / map.size;
+
+    context.clearRect(0, 0, size, size);
+    context.fillStyle = 'green';
+    context.fillRect(gridX * cellSize, gridY * cellSize, 10, 10);
+  }
+
   render() {
     const { size } = this.props;
-    return <canvas ref={this.canvasRef} height={size} width={size} />;
+    return (
+      <div style={styles.container}>
+        <canvas ref={this.wallsRef} height={size} width={size} style={styles.layer} />
+        <canvas ref={this.playerRef} height={size} width={size} style={styles.layer} />
+      </div>
+    );
   }
+}
+
+const styles = {
+  container: {
+    position: 'relative',
+  },
+  layer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+  },
 }
