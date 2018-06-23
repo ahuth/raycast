0cb1d7b5b77ff14de6b04d7f839f7c0392aa6038
Andrew Huth <andrew@huth.me>
2018-06-23 11:31:38 -0700

    Split out objects we need in the scene and minimap

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -5,15 +5,20 @@ import Player from '../utils/player';
 import Scene from './scene';
 import { fromDegrees } from '../utils/radians';
 
+const fov = fromDegrees(60);
+const loop = new Loop();
+const map = new Map(64);
+const player = new Player(160, 160, fromDegrees(0));
+
 export default function App() {
   return (
     <div>
       <Scene
-        loop={new Loop()}
+        loop={loop}
         resolution={320}
-        fov={fromDegrees(60)}
-        map={new Map(64)}
-        player={new Player(160, 160, fromDegrees(0))}
+        fov={fov}
+        map={map}
+        player={player}
         height={400}
         width={740}
       />
