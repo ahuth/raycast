6a0469e6d64a0e3e8924e91e1b81d2a8769b45a3
Andrew Huth <andrew@huth.me>
2018-06-24 11:25:22 -0700

    Adjust formatting of Column component

--- src/components/column.jsx
+++ src/components/column.jsx
@@ -2,7 +2,13 @@ import Color from 'color';
 import React from 'react';
 
 export default function Column({
-  color, distance, mapHeight, number, resolution, screenHeight, screenWidth,
+  color,
+  distance,
+  mapHeight,
+  number,
+  resolution,
+  screenHeight,
+  screenWidth,
 }) {
   const height = Math.min(mapHeight / distance * 255, screenHeight);
   const width = screenWidth / resolution;
@@ -16,5 +22,6 @@ export default function Column({
     position: 'absolute',
     top,
   };
+
   return <div style={styles} />;
 }
