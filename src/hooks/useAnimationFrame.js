2bc27d88da6b68e9e695e91fdf1fbb754db37a45
Andrew Huth <andrew.huth@airbnb.com>
2019-05-08 10:38:39 -0700

    Don't worry about setting a ref for the animation frame callback
    Not sure why I did that in th efirst place. What's weird is that after removing that ref, the app performs MUCH better...

--- src/hooks/useAnimationFrame.js
+++ src/hooks/useAnimationFrame.js
@@ -1,23 +1,21 @@
 import { useEffect, useRef } from 'react';
 
 export default function useAnimationFrame(callback) {
-  const callbackRef = useRef(callback);
   const frameRef = useRef();
   const timestampRef = useRef();
 
   useEffect(() => {
     timestampRef.current = window.performance.now();
-    callbackRef.current = callback;
-  }, [callback]);
+  }, []);
 
   useEffect(() => {
     function loop(timestamp) {
       frameRef.current = requestAnimationFrame(loop);
-      callbackRef.current(timestamp - timestampRef.current);
+      callback(timestamp - timestampRef.current);
       timestampRef.current = timestamp;
     };
 
     frameRef.current = requestAnimationFrame(loop);
     return () => cancelAnimationFrame(frameRef.current);
-  }, []);
+  }, [callback]);
 };
